8ba5934d687fdf18f44edb562b6a156de5316cb9
Musab Bin Ubaid <joiya4636@gmail.com>
2024-07-14 12:21:32 -0700

    Initial commit

---
+++ Day1.js
@@ -0,0 +1,49 @@
+// var mynum=65;
+// console.log(mynum);
+// let mystring="Hello, world";
+// console.log(mystring);
+// const flag=false;
+// console.log(flag);
+// let num=65;
+// console.log(typeof num);
+// let num2=65.6;
+// console.log(typeof num2);
+// let num3="Hello";
+// console.log(typeof num3);
+// let array=[1,3,4,5,5];
+// console.log(typeof array);
+// let obj={
+//     name:"John",
+//     age:23
+// }
+// console.log(typeof obj);
+// let number=17;
+// number=12;
+// console.log(number);
+// const constant=87;
+// constant=13;
+// console.log(constant);
+// Feature
+let num=65;
+console.log(num);
+console.log(typeof num);
+let num2=65.6;
+console.log(typeof num2);
+console.log(num2);
+let num3="Hello";
+console.log(typeof num3);
+console.log(num3);
+let array=[1,3,4,5,5];
+console.log(typeof array);
+console.log(array);
+let obj={
+    name:"John",
+    age:23
+}
+console.log(typeof obj);
+console.log(obj);
+/*
+Difference b/w let and const
+we reassign the value in let 
+we cannot ressign the value in const
+ */
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+var mynum=65;
+// console.log(mynum);
+// let mystring="Hello, world";
+// console.log(mystring);
+// const flag=false;
+// console.log(flag);
