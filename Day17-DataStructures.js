f05a8c02093c8449f274e33dd0c85922f5625559
Musab Bin Ubaid <joiya4636@gmail.com>
2024-07-29 14:11:55 -0700

    Update Day17-DataStructures.js

--- Day17-DataStructures.js
+++ Day17-DataStructures.js
@@ -4,67 +4,178 @@ class Node {
     this.next = null;
   }
 }
-class LinkedList {
+// class LinkedList {
+//   constructor() {
+//     this.head = null;
+//     this.size = 0;
+//   }
+//   addnode(data) {
+//     const node = new Node(data);
+//     if (this.head == null) {
+//       this.head = node;
+//       return;
+//     }
+
+//     node.next = this.head;
+//     this.head = node;
+//     this.size++;
+//   }
+//   addatlast(data) {
+//     const node = new Node(data);
+//     if (this.head == null) {
+//       this.head = node;
+//       this.size++;
+//       return;
+//     }
+//     let current = this.head;
+//     while (current.next != null) {
+//       current = current.next;
+//     }
+//     current.next = node;
+//     this.size++;
+//   }
+//   removefromlast() {
+//     if (this.head == null) {
+//       return;
+//     }
+//     let current = this.head;
+//     while (current.next.next != null) {
+//       current = current.next;
+//     }
+//     current.next = null;
+//     this.size--;
+//   }
+//   displaylist() {
+//     let current = this.head;
+//     while (current != null) {
+//       console.log(current.data, " ");
+//       current = current.next;
+//     }
+//   }
+//   displaysize() {
+//     console.log("Size: " + this.size);
+//   }
+// }
+class Stack {
   constructor() {
-    this.head = null;
-    this.size = 0;
+    this.top = null;
   }
-  addnode(data) {
-    const node = new Node(data);
-    if (this.head == null) {
-      this.head = node;
+  push(data) {
+    let node = new Node(data);
+    if (!this.top) {
+      this.top = node;
       return;
     }
-
-    node.next = this.head;
-    this.head = node;
-    this.size++;
+    node.next = this.top;
+    this.top = node;
   }
-  addatlast(data){
-    const node = new Node(data);
-    if (this.head == null) {
-        this.head = node;
-        this.size++;
-        return;
-      }
-     let current=this.head;
-     while(current.next != null){
-        current = current.next
-     }
-     current.next=node;
-     this.size++;
-  }
-  removefromlast(){
-    if(this.head==null){
-        return;
+  pop() {
+    if (this.top == null) {
+      return;
     }
-    let current=this.head;
-    while(current.next.next != null){
-        current = current.next
+    const data = this.top.data;
+    this.top = this.top.next;
+    return data;
+  }
+  peek() {
+    if (this.top == null) {
+      console.log("No element found ");
+      return;
     }
-    current.next=null;
-    this.size--;
+    return this.top.data;
   }
-  displaylist(){
-    let current=this.head;
-    while(current != null){
-        console.log(current.data, " ")
-        current = current.next;
+  isEmpty() {
+    if (this.top == null) {
+      return true;
     }
+    return false;
   }
-  displaysize(){
-    console.log("Size: " + this.size);
-  }
-
 }
-let L= new LinkedList();
-L.addatlast(5);
-L.addatlast(5);
-L.addatlast(5);
-L.addatlast(5);
-L.addnode(3);
-L.removefromlast();
-L.removefromlast();
-L.removefromlast();
-L.displaylist();
-L.displaysize();
+
+// function reverse(s) {
+//   let stack = new Stack();
+//   for (let char of s) {
+//     stack.push(char);
+//   }
+//   let reversed = "";
+//   while (!stack.isEmpty()) {
+//     reversed += stack.pop();
+//   }
+//   return reversed;
+// }
+
+// console.log(reverse("Musab"));
+
+// let L = new LinkedList();
+// L.addatlast(5);
+// L.addatlast(5);
+// L.addatlast(5);
+// L.addatlast(5);
+// L.addnode(3);
+// L.removefromlast();
+// L.removefromlast();
+// L.removefromlast();
+// L.displaylist();
+// L.displaysize();
+// let s = new Stack();
+// s.push(5);
+// s.pop();
+// s.peek();
+// s.push(6);
+// s.push(7);
+// s.push(8);
+// console.log(s.peek());
+// console.log(s.isEmpty());
+class queue {
+  constructor() {
+    this.front = null;
+    this.back = null;
+  }
+  enqueue(data) {
+    const node = new Node(data);
+    if (!this.front) {
+      this.front = node;
+      this.back = node;
+    }
+    this.back.next = node;
+    this.back = node;
+  }
+  dequeue() {
+    if (!this.front) {
+      return "Cannot Dequeue";
+    }
+    const data = this.front.data;
+    this.front = this.front.next;
+    if (this.front == null) {
+      this.back = null;
+    }
+    return data;
+  }
+  isEmpty() {
+    if (this.front == null) {
+      return true;
+    }
+    return false;
+  }
+  peek() {
+    if (this.front == null) {
+      return "Cannot Peek";
+    }
+    return this.front.data;
+  }
+}
+// let q = new queue();
+// q.enqueue(5);
+// q.enqueue(8);
+// console.log(q.dequeue());
+// console.log(q.dequeue());
+// console.log(q.isEmpty());
+function printer(jobs){
+  let q=new queue();
+  jobs.forEach(job=>q.enqueue(job));
+while(!q.isEmpty()){
+  console.log("Processed Job: "+q.dequeue());
+}
+}
+const jobs = ["Job1", "Job2", "Job3", "Job4"];
+printer(jobs);
